9acec93b97c44c008c561c5bf2398d213c321b1e
Thomas Malt <thomas@malt.no>
2014-06-09 23:01:21 +0200

    cleaned up file. Added health check api

--- app.js
+++ app.js
@@ -8,22 +8,36 @@
  * Copyright (c) 2013-2014 Thomas Malt <thomas@malt.no>
  *
  * @author Thomas Malt <thomas@malt.no>
- * @copyright Thommas Malt <thomas@malt.no>
+ * @copyright Thomas Malt <thomas@malt.no>
  */
 
-var power   = require('./lib/power'), 
-    config  = require('./config');
-    logger  = require('winston'), 
-    express = require('express'),
-    u       = require('underscore'),
-    events  = require('events'),
-    path    = require('path');
+/**
+ * jslint settings
+ */
+/*jslint node: true */
+/*jslint ass: true */
+/*jslint sloppy: true */
+/*jslint vars: true */
+
+var power      = require('./lib/power'),
+    config     = require('./config'),
+    logger     = require('winston'),
+    express    = require('express'),
+    u          = require('underscore'),
+    events     = require('events'),
+    VitalSigns = require('vitalsigns'),
+    path       = require('path');
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize:  true, 
+    colorize:  true,
     timestamp: true
 });
+
+var vitals = new VitalSigns();
+vitals.monitor('cpu');
+vitals.monitor('mem', {units: 'MB'});
+vitals.monitor('tick');
 
 var ctrl = power.controller;
 var app  = express();
@@ -34,6 +48,7 @@ app.use(express.bodyParser());
 
 // Code to implement rudimentary CORS support.
 app.all('*', function (req, res, next) {
+    "use strict";
     var origin = req.header('Origin');
     var index  = config.corsDomains.indexOf(origin);
 
@@ -41,11 +56,11 @@ app.all('*', function (req, res, next) {
 
     if (index > -1) {
         res.header(
-            "Access-Control-Allow-Origin", 
-            "*" 
+            "Access-Control-Allow-Origin",
+            "*"
         );
         res.header(
-            "Access-Control-Allow-Headers", 
+            "Access-Control-Allow-Headers",
             "X-Requested-With, Content-Type"
         );
         res.header("Access-Control-Max-Age", 600);
@@ -54,20 +69,20 @@ app.all('*', function (req, res, next) {
     next();
 });
 
+app.get('/power/health', vitals.express);
 
 app.get('/power/watts/:interval?', function (req, res) {
     if (req.params.interval === undefined || req.params.interval.match(/[0-9]+/)) {
-        var interval = parseInt(req.params.interval) || 5;
-        ctrl.watts.get(interval).then( function (body) {
+        var interval = parseInt(req.params.interval, 10) || 5;
+        ctrl.watts.get(interval).then(function (body) {
             logger.info("Got body returned as promised: ", body.length);
             res.setHeader('Cache-Control', 'public, max-age=1');
             res.setHeader('Content-Type', 'application/json');
             res.setHeader('Content-Length', body.length);
             res.end(body);
         });
-    }
-    else if (req.params.interval.match(/^hour$/)) {
-        ctrl.watts.hour.get(req.params.type).then( function (body) {
+    } else if (req.params.interval.match(/^hour$/)) {
+        ctrl.watts.hour.get(req.params.type).then(function (body) {
             logger.info("Got body returned as promised");
             res.setHeader('Cache-Control', 'public, max-age=4');
             res.setHeader('Content-Type', 'application/json');
@@ -82,16 +97,16 @@ app.get('/power/kwh/:type/:count?', function (req, res) {
     // req.params.resolution
     var maxage = {
         "today": 60,
-        "hour":  5*60,
-        "day":   10*60
+        "hour":  5 * 60,
+        "day":   10 * 60
     };
-    
+
     var type  = req.params.type;
     var count = req.params.count || 1;
 
-    ctrl.kwh.handler(type, count).then(function (body) { 
+    ctrl.kwh.handler(type, count).then(function (body) {
         logger.info("Got body returned as promised: ", body.length);
-        res.setHeader('Cache-Control', 'public, max-age='+maxage[type]);
+        res.setHeader('Cache-Control', 'public, max-age=' + maxage[type]);
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Content-Length', body.length);
         res.end(body);
@@ -108,6 +123,7 @@ app.put('/power/meter/total', function (req, res) {
     });
 });
 
+/*jslint unparam: true */
 app.get('/power/meter/total', function (req, res) {
     logger.info("Got get request to /meter/total");
     res.setHeader('Cache-Control', 'public, max-age=6');
@@ -117,6 +133,7 @@ app.get('/power/meter/total', function (req, res) {
         res.end(body);
     });
 });
+/*jslint unparam: false */
 
 app.listen(config.server.port || 3000);
 
