9c42ef9e1abbff4e6cf0c34e6f329868341caad8
Thomas Malt <thomas@malt.no>
2015-03-22 15:24:08 +0100

    setting up travis-ci and wallaby.js

---
+++ wallaby.js
@@ -0,0 +1,28 @@
+module.exports = function () {
+    console.log(process.cwd());
+    return {
+        files: [
+            "config.js",
+            "config-dev.js",
+            "package.json",
+            "app.js",
+            "lib/*.js"
+        ],
+
+        tests: [
+            "test/*Test.js"
+        ],
+
+        env: {
+            type: "node",
+            runner: "/usr/local/bin/node",
+            params: {
+                env: "POWER_ENV=test; TZ=Europe/Oslo"
+            }
+        },
+
+        workers: {
+            recycle: false
+        }
+    }
+};
