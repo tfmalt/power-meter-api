6dca2f24196bf1f1c536b086270d8d1b345e2637
Thomas Malt <thomas@malt.no>
2016-02-13 19:46:42 +0100

    moved to stateless configuraiton

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-config-prod.js
+
+exports.server = {
+    port: process.env.PORT || 3000
+};
+
+exports.redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+exports.corsDomains = process.env.CORS_DOMAINS.split(' ') || "http://localhost";
+
