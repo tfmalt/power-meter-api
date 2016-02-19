0b1cf11d6d609bb00afdb09fcda53c47047b77d4
Thomas Malt <thomas@malt.no>
2016-02-19 20:09:02 +0100

    Updated configuration parsing

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ exports.server = {
     port: process.env.PORT || 3000
 };
 
-exports.redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
-exports.corsDomains = process.env.CORS_DOMAINS.split(' ') || "http://localhost";
+exports.environment = process.env.POWER_ENV || "production";
+exports.redisUrl    = process.env.REDIS_URL || "redis://localhost:6379";
+exports.corsDomains = [];
 
+if (process.env.CORS_DOMAINS) {
+    exports.corsDomains = process.env.CORS_DOMAINS.split(' ') || ["http://localhost"];
+}
