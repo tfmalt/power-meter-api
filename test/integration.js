f52424392ebd9706f3abbb77130ec91e50644a3d
Thomas Malt <thomas@malt.no>
2017-03-31 12:12:28 +0200

    improving integration testing

--- test/integration.js
+++ test/integration.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const http = require('chai-http');
 const app  = require('../server').app;
+const version = require('../package').version;
 const expect = chai.expect;
 
 chai.use(http);
@@ -12,6 +13,44 @@ describe('Get a correct response from server', () => {
     chai.request(app).get('/').end((err, res) => {
       expect(err).to.be.an.object;
       expect(res).to.have.status(404);
+      done();
+    });
+  });
+});
+
+describe('power health is working', () => {
+  it('should return correct health json', (done) => {
+    chai.request(app).get('/power/health').end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.be.json;
+      expect(res).to.not.have.header('x-powered-by');
+      expect(res.body).to.contain.keys(['cpu', 'mem', 'tick', 'healthy']);
+      done();
+    });
+  });
+});
+
+describe('power API', () => {
+  it('should return correct version information', (done) => {
+    chai.request(app).get('/power/').end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.be.json;
+      expect(res.body).to.deep.equal({
+        message: 'Service is running properly',
+        version: 'v' + version
+      });
+      done();
+    });
+  });
+});
+
+describe('/power/watts', () => {
+  it('should return correct watts for default interval', (done) => {
+    chai.request(app).get('/power/watts').end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.be.json;
+      expect(res).to.have.header('Cache-Control', 'public, max-age=5');
+      console.log(res.body);
       done();
     });
   });
