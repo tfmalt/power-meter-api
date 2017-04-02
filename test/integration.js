9b371a8a7360c490ef1fe5fdc530de58330149f3
Thomas Malt <thomas@malt.no>
2017-04-02 16:46:19 +0200

    improving integration tests

--- test/integration.js
+++ test/integration.js
@@ -85,14 +85,54 @@ describe('/power/watts', () => {
   });
 });
 
-describe('/kwh/date/:year?/:month?/:date?', (done) => {
+describe('/kwh/date/:year?/:month?/:date?', () => {
   it('should return valid json for a valid date', (done) => {
     chai.request(app).get('/power/kwh/date/2017/03/30').end((err, res) => {
       expect(err).to.be.null;
       expect(res).to.be.json;
-      // expect(res).to.have.header('Cache-Control', 'public, max-age=30');
-      console.log(res.body);
+      expect(res.body).to.contain.keys({kwh: 77.3696, pulses: 773696});
+      expect(res.body).to.contain.keys(['perHour', 'description', 'time']);
+      expect(res.body.perHour.length).to.equal(24);
       done();
     });
   });
+
+  it('should return proper error code for a year in the future', (done) => {
+    const year = (new Date()).getFullYear() + 1;
+
+    chai.request(app).get(`/power/kwh/date/${year}/03/30`).end((err, res) => {
+      expect(err).to.be.object;
+      expect(res).to.be.json;
+      expect(res).to.have.status(400);
+      expect(res.body.message).to.match(/Year must be between/);
+      done();
+    });
+  });
+
+  it('should return proper error code for a month in the future', (done) => {
+    const date  = new Date();
+    const year  = date.getFullYear();
+    const month = '0' + (date.getMonth() + 3);
+
+    chai.request(app).get(`/power/kwh/date/${year}/${month}/10`).end((err, res) => {
+      // console.log(res.status);
+      // console.log(res.body);
+      expect(err).to.be.object;
+      expect(res).to.be.json;
+      expect(res).to.have.status(400);
+      expect(res.body.message).to.match(/Date is in the future/);
+      done();
+    });
+  });
+
+  it('should return valid json for a valid month', (done) => {
+    chai.request(app).get('/power/kwh/date/2017/03').end((err, res) => {
+      console.log(res.status);
+      console.log(res.body);
+      //  expect(err).to.be.null;
+      expect(res).to.be.json;
+      done();
+    });
+  });
+
 });
