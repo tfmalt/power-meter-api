d27fa1762c53f47dafaf5a41735b7f3c0c09a39c
Thomas Malt <thomas@malt.no>
2017-04-03 18:48:23 +0200

    improving tests

--- test/PowerMeterController.test.js
+++ test/PowerMeterController.test.js
@@ -7,11 +7,24 @@ const PowerMC = require('../lib/power-meter-controller');
 const pmc = new PowerMC();
 
 describe('power-meter-controller unit-tests', () => {
-  it('should be an instance of PoowerMeterController', () => {
+  it('should be an instance of PowerMeterController', () => {
     expect(pmc).to.be.instanceof(PowerMC);
   });
 
   it('handleKwh should throw error when called with invalid data', () => {
     expect(pmc.handleKwh.bind(pmc, 'tullball')).to.throw(TypeError, /first argument must be/);
+  });
+
+  it('normalizeDate should throw error on wrong input', () => {
+    expect(pmc.normalizeDate.bind(pmc, new Date(), 'tullball'))
+      .to.throw(Error, /parameter level not set/);
+  });
+
+  it('normalizeDate should return date when level is hour', () => {
+    expect(pmc.normalizeDate(new Date(), 'hour')).to.be.instanceof(Date);
+  });
+
+  it('normalizeDate should return date when level is week', () => {
+    expect(pmc.normalizeDate(new Date(), 'week')).to.be.instanceof(Date);
   });
 });
