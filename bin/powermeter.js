6bdaccd6b31bf54aa0a18d57bbc90da9982546e8
Thomas Malt <thomas@malt.no>
2017-04-10 15:59:58 +0200

    nicer syntax

--- bin/powermeter.js
+++ bin/powermeter.js
@@ -9,45 +9,46 @@ const args = require('args');
 const fetch = require('node-fetch');
 const chalk = require('chalk');
 
-const key = process.env.POWER_API_KEY;
-const secret = process.env.POWER_API_SECRET;
-
-args.options([
-  {
-    name: 'uri',
-    description: 'The base uri to use for requests.',
-    defaultValue: 'http://localhost:3000'
-  },
-  {
-    name: 'key',
-    description: 'API key to access the API',
-    defaultValue: null
-  }
-]);
+args
+  .option('uri', 'The base uri to use for requests.', 'https://api.malt.no')
+  .option('apikey', 'API key to access the API', process.env.POWER_APIKEY);
 
 args.command('usage', 'Current electricity usage.', (name, sub, options) => {
-  const url = options.uri + '/power/kwh/seconds/3600';
-  fetch(url)
+  if (typeof options.apikey !== 'string' || options.apikey.length !== 32) {
+    console.error(
+      'Error: ' + chalk.red('missing argument') + '. ' +
+      'Could not find a valid ' + chalk.red('apikey') + '.'
+    );
+    process.exit(1);
+  }
+  const url  = options.uri + '/power/kwh/seconds/3600';
+  const opts = {
+    headers: {
+      apikey: options.apikey
+    }
+  };
+  fetch(url, opts)
     .then(res => res.json())
     .then(json => {
-      console.log(chalk.dim('                max  avg  min'));
       console.log(
         'Load last hour:',
-        chalk.cyan(json.summary.watts.max),
-        chalk.cyan.underline(json.summary.watts.average),
-        chalk.cyan(json.summary.watts.min),
-        'Watt.'
+        chalk.dim('max:') + chalk.cyan(json.summary.watts.max) + ', ' +
+        chalk.dim('avg:') + chalk.cyan.underline(json.summary.watts.average) + ', ' +
+        chalk.dim('min:') + chalk.cyan(json.summary.watts.min) + ' Watt.'
       );
       console.log(
         'Total usage last hour:',
         chalk.cyan(json.summary.kwh.total),
         'kWh.');
+    })
+    .catch(error => {
+      console.log(error.toString());
     });
 
-  fetch(options.uri + '/power/watts')
+  fetch(options.uri + '/power/watts', opts)
     .then(res => res.json())
     .then(json => {
-      console.log('Right now the load is', chalk.cyan(json.watt), 'Watt.');
+      console.log('Load right now:', chalk.cyan(json.watt), 'Watt.');
     })
     .catch(err => {
       console.error('Error:', err.name, err.message);
